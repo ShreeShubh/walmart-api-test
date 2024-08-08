6432325f5279b7dbdde660333067d3e82d9ccc5b
Shubham Shree <shubhamshree@iVMBAM2amLaptop.localdomain>
2024-08-08 16:50:55 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,234 @@
-import Image from "next/image";
+"use client"
+
+import React, { useState } from "react"
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [formData, setFormData] = useState({
+    yourBusiness: "",
+    yourType: "",
+    yourName: "",
+    yourSector: "",
+    yourState: "",
+    yourCity: "",
+    yourPhone: "",
+    yourEmail: "",
+    yourExporting: "",
+    youreCommerce: "",
+    selectedSource: "",
+    otherSource: "",
+    yourDate: "",
+    utm_source: "abc",
+    utm_medium: "abc",
+    utm_campaign: "abc",
+    utm_id: "abc",
+    recaptchaToken: "abcd",
+  })
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    try {
+      const response = await fetch("https://api.ivista.biz/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      })
+
+      if (response.ok) {
+        console.log("Form submitted successfully!")
+        // Handle success (e.g., show a success message, clear the form, etc.)
+      } else {
+        console.error("Failed to submit the form")
+        // Handle error (e.g., show an error message)
+      }
+    } catch (error) {
+      console.error("Error:", error)
+      // Handle error (e.g., show an error message)
+    }
+  }
+
+  return (
+    <form className="p-20 text-black" onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <input
+          type="text"
+          name="yourBusiness"
+          value={formData.yourBusiness}
+          onChange={handleChange}
+          placeholder="Your Business"
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="mb-3">
+        <input
+          type="text"
+          name="yourType"
+          value={formData.yourType}
+          onChange={handleChange}
+          placeholder="Your Type"
+        />
       </div>
-    </main>
-  );
+
+      <div className="mb-3">
+        <input
+          type="text"
+          name="yourName"
+          value={formData.yourName}
+          onChange={handleChange}
+          placeholder="Your Name"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          type="text"
+          name="yourSector"
+          value={formData.yourSector}
+          onChange={handleChange}
+          placeholder="Your Sector"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          type="text"
+          name="yourState"
+          value={formData.yourState}
+          onChange={handleChange}
+          placeholder="Your State"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          type="text"
+          name="yourCity"
+          value={formData.yourCity}
+          onChange={handleChange}
+          placeholder="Your City"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          type="text"
+          name="yourPhone"
+          value={formData.yourPhone}
+          onChange={handleChange}
+          placeholder="Your Phone"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          type="email"
+          name="yourEmail"
+          value={formData.yourEmail}
+          onChange={handleChange}
+          placeholder="Your Email"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          type="text"
+          name="yourExporting"
+          value={formData.yourExporting}
+          onChange={handleChange}
+          placeholder="Are You Exporting?"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          type="text"
+          name="youreCommerce"
+          value={formData.youreCommerce}
+          onChange={handleChange}
+          placeholder="Are You Using eCommerce?"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          type="text"
+          name="selectedSource"
+          value={formData.selectedSource}
+          onChange={handleChange}
+          placeholder="Selected Source"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          type="text"
+          name="otherSource"
+          value={formData.otherSource}
+          onChange={handleChange}
+          placeholder="Other Source"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          type="date"
+          name="yourDate"
+          value={formData.yourDate}
+          onChange={handleChange}
+          placeholder="Your Date"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          type="text"
+          name="utm_source"
+          value={formData.utm_source}
+          onChange={handleChange}
+          placeholder="UTM Source"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          type="text"
+          name="utm_medium"
+          value={formData.utm_medium}
+          onChange={handleChange}
+          placeholder="UTM Medium"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          type="text"
+          name="utm_campaign"
+          value={formData.utm_campaign}
+          onChange={handleChange}
+          placeholder="UTM Campaign"
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          type="text"
+          name="utm_id"
+          value={formData.utm_id}
+          onChange={handleChange}
+          placeholder="UTM ID"
+        />
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  )
 }
